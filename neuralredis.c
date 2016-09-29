0331e80113af89fb0f6d94cb775129c54969fb9b
antirez <antirez@gmail.com>
2016-09-29 19:26:45 +0200

    License added.

--- neuralredis.c
+++ neuralredis.c
@@ -5,7 +5,7 @@
  *
  * -----------------------------------------------------------------------------
  *
- * Copyright (c) 2016, Salvatore Sanfilippo <antirez at gmail dot com>
+ * Copyright (C) 2016, Salvatore Sanfilippo <antirez at gmail dot com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
